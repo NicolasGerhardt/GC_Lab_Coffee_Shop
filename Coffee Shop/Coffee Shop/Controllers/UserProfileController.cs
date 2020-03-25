8d53f803995d7c8d2256f7cf24e4794d315fb331
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-03-25 08:34:00 -0400

    Completed base requirements

---
+++ Coffee Shop/Coffee Shop/Controllers/UserProfileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee_Shop.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        [HttpPost]
+        public IActionResult Summary(string fname, string lname, string email, string password)
+        {
+            ViewData["fname"] = fname;
+            ViewData["lname"] = lname;
+            ViewData["email"] = email;
+            ViewData["password"] = password;
+            return View();
+        }
+    }
+}
